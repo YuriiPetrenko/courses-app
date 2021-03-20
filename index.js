d189d829f0e48c9499e84780b6ea1a2233f0ae26
yuriipetrenko <obzora6@gmail.com>
2021-03-20 19:54:58 +0200

    add  MongoDB connection and update add & course

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const path = require('path')
 const app = express()
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
+const mongoose = require('mongoose')
 const addRoutes = require('./routes/add')
 const courseRoutes = require('./routes/courses')
 const cardRoute = require('./routes/card')
 
 //configure handlebars
 const hbs = exphbs.create({
-         defaultLayout: 'main',
-         extname: 'hbs'
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
 app.engine('hbs', hbs.engine)
@@ -27,8 +28,20 @@ app.use('/add',addRoutes)
 app.use('/courses',courseRoutes)
 app.use('/card', cardRoute)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
+async function start() {
+  try {
 
-app.listen(PORT, ()=> {
-         console.log(`Server has been started on port: ${PORT}`)
-})
+    const url = 'mongodb+srv://yurii:2cAbstiUiKTi91LY@cluster0.ahyc9.mongodb.net/shop?retryWrites=true&w=majority'
+    await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+//2cAbstiUiKTi91LY
--- routes/add.js
+++ routes/add.js
@@ -1,19 +1,27 @@
 const {Router} = require('express')
-const router = Router()
 const Course = require('../models/course')
+const router = Router()
 
-router.get('/', (req,res) => {
-    res.render('add', {
-        title: 'Add new course',
-        isAdd: true
-    })
+router.get('/', (req, res) => {
+  res.render('add', {
+    title: 'Add course',
+    isAdd: true
+  })
 })
 
-router.post('/', async (req, res)=>{
-    const course = new Course(req.body.title, req.body.price, req.body.img);
+router.post('/', async (req, res) => {
+  const course = new Course({
+    title: req.body.title,
+    price: req.body.price,
+    img: req.body.img
+  })
 
+  try {
     await course.save()
     res.redirect('/courses')
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 module.exports = router
