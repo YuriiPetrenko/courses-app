79bc03908cec426671664791d49e4a5c5a26c77a
yuriipetrenko <obzora6@gmail.com>
2021-03-22 13:57:36 +0200

    update cart using MongoDB

--- models/user.js
+++ models/user.js
@@ -25,6 +25,38 @@ const userSchema = new Schema({
             }
         ]
     }
-}) 
+})
+
+userSchema.methods.addToCart = function(course){
+    const clonedItems = [...this.cart.items]
+    const index = clonedItems.findIndex(c => c.courseId.toString() === course._id.toString())
+
+    if (index>=0){
+        clonedItems[index].count += 1
+    }else{
+        clonedItems.push({
+            courseId: course._id,
+            count: 1
+        })
+    }
+
+    const newCart = {items: clonedItems}
+    this.cart = newCart
+    return this.save()
+}
+
+userSchema.methods.removeFromCart = function(id){
+    let items = [...this.cart.items]
+    const index = items.findIndex(c => c.courseId.toString() === id.toString())
+
+    if(items[index].count === 1){
+        items = items.filter(item => item.courseId.toString() !== id.toString())
+    }else{
+        items[index].count--
+    }
+
+    this.cart = {items}
+    return this.save()
+}
 
 module.exports = model('User',userSchema)
